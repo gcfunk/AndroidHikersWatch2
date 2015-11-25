132bbd8ff184ee22c7e4d6742fa053573bfb428a
Greg Funk <greg.funk@walltowall.com>
2015-11-25 10:56:27 -0500

    Gets several pieces of info from location

--- app/src/main/java/com/example/gregfunk/androidhikerswatch2/MainActivity.java
+++ app/src/main/java/com/example/gregfunk/androidhikerswatch2/MainActivity.java
@@ -1,13 +1,72 @@
 package com.example.gregfunk.androidhikerswatch2;
 
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener {
+
+    LocationManager locationManager;
+    String provider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        provider = locationManager.getBestProvider(new Criteria(), false);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        locationManager.removeUpdates(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        locationManager.requestLocationUpdates(provider, 400, 1, this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Double lat = location.getLatitude();
+        Double lng = location.getLongitude();
+        Double alt = location.getAltitude();
+        float bearing = location.getBearing();
+        float speed = location.getSpeed();
+        float accuracy = location.getAccuracy();
+
+        Log.i("Latitude", String.valueOf(lat));
+        Log.i("lng", String.valueOf(lng));
+        Log.i("altitude", String.valueOf(alt));
+        Log.i("bearing", String.valueOf(bearing));
+        Log.i("speed", String.valueOf(speed));
+        Log.i("accuracy", String.valueOf(accuracy));
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 }
